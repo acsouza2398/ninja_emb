a9830c96fd78383ad73b549868e71c5daeb54c70
acsouza2398 <63055644+acsouza2398@users.noreply.github.com>
2022-11-11 08:56:51 -0300

    Update

--- parser.py
+++ parser.py
@@ -1,5 +1,3 @@
-import os
-
 def parser():
     with open("lab1.txt", "r") as f:
         lines = f.readlines()
@@ -8,8 +6,4 @@ def parser():
         else:
             return False
 
-name = 'log'
-value = parser()
-
-with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-    print(f'{name}={value}', file=fh)
+parser()
